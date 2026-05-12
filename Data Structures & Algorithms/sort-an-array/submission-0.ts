c588521a335008ce211672780a4895a54a113b4b
Erick Marcondes de Mattos Carpes <erickcarpesmmc@gmail.com>
2026-05-12 16:37:56 -0300

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums: number[]): number[] {
+        if(nums.length <= 1) return nums;
+
+        const left = this.sortArray(nums.slice(0, nums.length / 2));
+        const right = this.sortArray(nums.slice(nums.length / 2, nums.length));
+
+        let l = 0, r = 0;
+        const output = [];
+        while(l < left.length && r < right.length) {
+            if(left[l] < right[r]) {
+                output.push(left[l])
+                l++;
+            } else {
+                output.push(right[r])
+                r++;
+            }
+        }
+        
+        while(l < left.length) {
+            output.push(left[l])
+            l++;
+        }
+
+        while(r < right.length) {
+            output.push(right[r])
+            r++;
+        }
+
+        return output;
+    }
+}
