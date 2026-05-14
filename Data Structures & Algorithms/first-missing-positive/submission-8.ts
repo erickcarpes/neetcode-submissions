1e5d45665167c314fdfb202e5f5219ab0fee1f7f
Erick Marcondes de Mattos Carpes <erickcarpesmmc@gmail.com>
2026-05-14 10:33:46 -0300

    Add: first-missing-positive - submission-8

---
+++ Data Structures & Algorithms/first-missing-positive/submission-8.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums: number[]): number {
+        for(let i=0;i<nums.length;i++){
+            while(nums[i] > 0 && nums[i] <= nums.length && nums[i] !== nums[nums[i] - 1]) {
+                const temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for(let i=0;i<nums.length;i++){
+            if(nums[i] !== i + 1) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+}
